bfa720a82c72ab650f1d424e357c05ee7710df31
theme222 <sira.tongsima@yahoo.com>
2025-11-24 21:56:17 +0700

    2025-11-24 21:56:17 Commit and push

--- DataStructuresAndAlgorithms/a/ex02e3_bs.cpp
+++ DataStructuresAndAlgorithms/a/ex02e3_bs.cpp
@@ -1,36 +1,38 @@
 #include <iostream>
 #include <vector>
 
+std::vector<int> vec;
+
+int bsearch()
+{
+    int currNum;
+    std::cin >> currNum;
+
+    int l = 0, r = vec.size(); // [l, r) so no ans
+
+    while (l < r)
+    {
+        // std::cout << l << ' ' << r << '\n';
+        int mid = l + (r-l)/2;
+        if (vec[mid] <= currNum) // since its upper bound so <=
+            l = mid + 1;
+        else
+            r = mid; // since its [l, r)
+    }
+
+    return l - 1;
+}
+
 int main()
 {
     int n, m;
     std::cin >> n >> m;
-    std::vector<int> vec(n);
-    for (auto &a: vec) std::cin >> a;
-    
+    vec.resize(n); for (auto& a: vec) std::cin >> a;
+
     while (m--)
     {
-        int currNum;
-        std::cin >> currNum;
-        
-        int l = 0, r = vec.size();
-        
-        while (l < r)
-        {
-            std::cout << l << ' ' << r 
-            int mid = l + ((r-l)/2);
-            if (vec[mid] == currNum) 
-                l = r = mid;
-            else if (vec[mid] > currNum) 
-                r = mid-1;
-            else if (vec[mid] < currNum)
-                l = mid;
-        }
-        
-        if (vec[l] == currNum) std::cout << vec[l] << '\n';
-        else if (l == 0) std::cout << "-1\n";
-        else std::cout << vec[l-1] << '\n';
+        std::cout << bsearch() << '\n';
     }
 
-    
+
 }
