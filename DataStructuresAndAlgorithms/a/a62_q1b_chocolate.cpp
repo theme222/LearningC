398f2640341c6a620934a5ca082e65c27feabc63
theme222 <sira.tongsima@yahoo.com>
2025-11-25 21:57:54 +0700

    2025-11-25 21:57:54 Commit and push

--- DataStructuresAndAlgorithms/a/a62_q1b_chocolate.cpp
+++ DataStructuresAndAlgorithms/a/a62_q1b_chocolate.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <vector>
+
+typedef long long ll;
+
+ll chocolateEatingDiary[69696];
+const ll sneakyMod = 1000003;
 
 int main()
 {
+    chocolateEatingDiary[0] = 1;
+    ll n, k;
+    std::cin >> n >> k;
     
+    std::vector<ll> eatingTypes(k);
+    for (auto &a: eatingTypes) std::cin >> a;
+    
+    for (ll i = 1; i <= n; i++)
+    {
+        int total = 0;
+        for (ll size: eatingTypes)
+        {
+            if (i < size) continue;
+            total += chocolateEatingDiary[i-size];
+        }
+        chocolateEatingDiary[i] = total % sneakyMod;
+    }
+    
+    std::cout << chocolateEatingDiary[n] << '\n';
 }
